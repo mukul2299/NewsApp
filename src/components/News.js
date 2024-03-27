8b2867704e01118bde2c6a0e05024667625f407d
mukulpandit <mukul2299pandit@gmail.com>
2024-03-27 10:58:22 +0530

    basic skeleton

---
+++ src/components/News.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class News extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default News
