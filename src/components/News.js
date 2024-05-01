79a81c4fa79e7a99d9f2976cf79399fba85bd03c
mukulpandit <mukul2299pandit@gmail.com>
2024-05-01 11:32:43 +0530

    Used news api server and pagination

--- src/components/News.js
+++ src/components/News.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import NewsItem from './NewsItem'
 
 export class News extends Component {
-  static propTypes = {
+    
+    constructor() {
+        super();
+        this.state = {
+            articles: [],
+            loading: false,
+            page:1,
+            pagesize:15,
+            totalResults:0
+        }
+    }
 
-  }
+    async componentDidMount(){
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=9bec86ac18cc4d888063062b2e49206e&pagesize=${this.state.pagesize}&page=${this.state.page}`;
+        let data = await fetch(url);
+        let parsedData= await data.json();
+        this.setState({
+            articles:parsedData.articles,
+            totalResults:parsedData.totalResults,
+            pagesize:15,
+            page:1,
+            loading:false
+        });
+    }
 
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    handlePrevious= async()=> {
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=9bec86ac18cc4d888063062b2e49206e&pagesize=${this.state.pagesize}&page=${this.state.page-1}`;
+        let data = await fetch(url);
+        let parsedData= await data.json();
+        this.setState({
+            articles:parsedData.articles,
+            page: this.state.page-1,
+        });
+    }
+
+    handleNext=async() =>{
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=9bec86ac18cc4d888063062b2e49206e&pagesize=${this.state.pagesize}&page=${this.state.page+1}`;
+        let data = await fetch(url);
+        let parsedData= await data.json();
+        this.setState({
+            articles:parsedData.articles,
+            page: this.state.page+1
+        });
+    }
+
+    render() {
+        return (
+            <div className="container my-3">
+                <h2>NewFrog - Top Headlines</h2>
+                <div className="container my-3">
+                    <div className="row md-4 my-3">
+                        {this.state.articles.filter((elem)=>{ return elem.title!=="[Removed]"}).map((elem) => {
+                            return <div key={elem.url} className="col md-1 my-3">
+                                <NewsItem title={elem.title} desc={elem.description} image={elem.urlToImage} newsUrl={elem.url} />
+                            </div>
+                        })}
+                    </div>
+                </div>
+                <div className='container d-flex justify-content-between'>
+                    <button type="button" disabled={this.state.page<=1} onClick={this.handlePrevious} class="btn btn-outline-dark">← Previous</button>
+                    <button type="button" disabled={this.state.totalResults<this.state.page*this.state.pagesize} onClick={this.handleNext} class="btn btn-outline-dark">Next →</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default News
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 export class NewsItem extends Component {
-  static propTypes = {
-
-  }
-
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    constructor(){
+        super();
+        console.log("NewsItem Component constructor")
+    }
+    render() {
+     let {title,image,desc,newsUrl}=this.props;
+        return (
+            <div>
+                <div className="card" style={{width: "18rem"}}>
+                    <img src={image?image:"https://lh3.googleusercontent.com/J6_coFbogxhRI9iM864NL_liGXvsQp2AupsKei7z0cNNfDvGUmWUy20nuUhkREQyrpY4bEeIBuc=s0-w300-rw"} height="150px" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 height="80px" className="card-title">{title===null? "Untitled":title.length>55 ? title.slice(0,55)+"...":title}</h5>
+                        <p height="100px" className="card-text">{desc===null? "No Description":desc.length>83 ? desc.slice(0,83)+"...":desc}</p>
+                        <a href={newsUrl} ref="noreferrer" target='_blank' className="btn btn-sm btn-primary">more</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default NewsItem
